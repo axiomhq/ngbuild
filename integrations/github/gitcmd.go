6d400d304e2ad3cf8cac2af39a2b54ec0177a91a
Gord Allott <gordallott@gmail.com>
2016-11-05 15:08:44 +0000

    added branch builds
    as of right now the github integration only builds pull requests
    this commit adds building of specific branches (generally master)
    whenever a commit is added to the branch

--- integrations/github/gitcmd.go
+++ integrations/github/gitcmd.go
@@ -14,16 +14,27 @@ func (g *Github) cloneAndMerge(directory string, config *core.BuildConfig) error
 	if baseBranch == "" {
 		baseBranch = "master"
 	}
+	script := ""
+	if config.GetMetadata("github:BuildType") == "pullrequest" {
+		if config.HeadRepo == "" || config.HeadHash == "" || config.BaseRepo == "" {
+			return errors.New("Config is not filled out properly")
+		}
 
-	if config.HeadRepo == "" || config.HeadHash == "" || config.BaseRepo == "" {
-		return errors.New("Config is not filled out properly")
+		script += fmt.Sprintf(`git clone -q --branch %s %s "%s"; `, baseBranch, config.BaseRepo, directory)
+		script += fmt.Sprintf(`cd %s ; `, directory)
+		script += fmt.Sprintf(`git remote add head %s ; `, config.HeadRepo)
+		script += fmt.Sprintf(`git fetch head ; `)
+		script += fmt.Sprintf(`git merge --no-edit --commit %s ; `, config.HeadHash)
+
+	} else if config.GetMetadata("github:BuildType") == "commit" {
+		if config.BaseRepo == "" || config.BaseHash == "" {
+			return errors.New("Config is not filled out properly")
+		}
+
+		script += fmt.Sprintf(`git clone -q --branch %s %s "%s"; `, baseBranch, config.BaseRepo, directory)
+		script += fmt.Sprintf(` cd %s ; `, directory)
+		script += fmt.Sprintf(`git checkout -q -f %s ; `, config.BaseHash)
 	}
-
-	script := fmt.Sprintf(`git clone -q --branch %s %s "%s"; `, baseBranch, config.BaseRepo, directory)
-	script += fmt.Sprintf(`cd %s ; `, directory)
-	script += fmt.Sprintf(`git remote add head %s ; `, config.HeadRepo)
-	script += fmt.Sprintf(`git fetch head ; `)
-	script += fmt.Sprintf(`git merge --no-edit --commit %s ; `, config.HeadHash)
 
 	cmd := exec.Command("/bin/sh", "-c", "-e", script)
 	output, err := cmd.Output()
@@ -31,6 +42,5 @@ func (g *Github) cloneAndMerge(directory string, config *core.BuildConfig) error
 		logcritf("Error cloning repo: \nscript: %s\nstdout: %s", script, string(output))
 		return err
 	}
-
 	return nil
 }
