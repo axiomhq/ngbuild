7a7df6cc0f5e725d51bdd301f432618667c68b71
Gord Allott <gordallott@gmail.com>
2016-11-13 12:38:04 +0000

    Add vendoring

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 set -e
+# never use system installed gopath
+GOPATH="`pwd`"
 
 # never make a directory called src, i will destroy it and your livelyhood
 rm -rf src
-mkdir -p src/github.com/watchly/
-ln -s "`pwd`" "`pwd`/src/github.com/watchly/ngbuild"
-GOPATH=$GOPATH:`pwd`
 
-go get -u github.com/watchly/ngbuild
 go get -u github.com/stretchr/testify
 
-go test -v -race ./...
+mkdir -p src/github.com/watchly/
+ln -s "`pwd`" "`pwd`/src/github.com/watchly/ngbuild"
+cd src/github.com/watchly/ngbuild 
+
+go test -v -race $(go list ./... | grep -v vendor)
